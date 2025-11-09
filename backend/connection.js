de68479df13f7e6839af943f1ea3b614a58333b0
SivakumarM-ks005 <sivakumar.mani@kryasolutions.com>
2025-11-09 13:04:28 +0530

    basic setup
    back end basic setup

---
+++ backend/connection.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+var  connection = mysql.createConnection({
+    port: process.env.DB_PORT,
+    host : process.env.DB_LOCALHOST,
+    user : process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((error)=>{
+    try {
+        console.log("connected")
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+module.exports= connection;
---
+++ backend/tables.sql
@@ -0,0 +1,27 @@
+create table user (
+    userId int primary key AUTO_INCREMENT,
+    userName varchar(250),
+    password varchar(250),
+    email varchar(100),
+    firstName varchar(100),
+    lastName varchar(100),
+    dateRegistered date 
+    lastLogin DATETIME 
+    role  varchar(20),
+    Status varchar(20),
+    UNIQUE(email)
+)
+
+create table user (
+    userId int primary key AUTO_INCREMENT,
+    userName varchar(250),
+    password varchar(250),
+    email varchar(100),
+    firstName varchar(100),
+    lastName varchar(100),
+    dateRegistered date 
+    lastLogin DATETIME 
+    role  varchar(20),
+    Status varchar(20),
+    UNIQUE(email)
+)
